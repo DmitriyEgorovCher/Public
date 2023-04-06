2c90b0c72da1f3af17dded7c731008891c15e219
DmitriyEgorovCher <125373055+DmitriyEgorovCher@users.noreply.github.com>
2023-04-06 12:48:55 +0300

    Delete 12.7.3 HW.py

--- 12.7.3 HW.py
+++
@@ -1,8 +0,0 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = int(input('введите сумму для внесения: '))
-deposit = []
-for key in per_cent:
-    deposit.append(per_cent[key] * money / 100)
-max_deposit = max(deposit)
-print(deposit)
-print('Максимальный депозит = ' + str(max_deposit))
