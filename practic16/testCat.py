59f83b6708e85d1c4689c6505bee5e8339203616
DmitriyEgorovCher <125373055+DmitriyEgorovCher@users.noreply.github.com>
2023-05-02 21:03:11 +0300

    Add files via upload

---
+++ practic16/testCat.py
@@ -0,0 +1,7 @@
+from classCat import Cat
+
+cat1 = Cat("Барон", "кот", 2)
+cat2 = Cat("Сэм", "кот", 2)
+
+print(f' Моё имя {cat1.name}, я {cat1.gender} и мне {cat1.age} года')
+print(f' Моё имя {cat2.name}, я {cat2.gender} и мне {cat2.age} года')
